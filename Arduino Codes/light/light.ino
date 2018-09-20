6ab69f741bf1085b448a18275ec67771449f5488
Irfan Mohammed <shaikhir19@gmail.com>
2018-09-20 21:10:21 +0530

    Add files via upload
    Basic Arduino codes

---
+++ Arduino Codes/light/light.ino
@@ -0,0 +1,30 @@
+int lightPin=0;
+int ledPin=2;
+int val;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val=analogRead(lightPin);
+Serial.print("Lux=");
+Serial.print(val);
+Serial.println();
+delay(800);
+if(val<100)
+{Serial.println("Dark");}
+else if (val<200)
+{Serial.println("less Dark");}
+else if (val<300)
+{Serial.println("Normal");}
+else if (val<400)
+{Serial.println("Lit");}
+else if (val<500)
+{Serial.println("Bright");}
+else
+{Serial.println("very Bright");}}
+
+
---
+++ Arduino Codes/motor/motor.c.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+byte sensorPin = 0;
+byte motorPin = 3;
+int prev = 0;
+Servo motor;
+
+void setup() {
+  // put your setup code here, to run once:
+  motor.attach(motorPin);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(sensorPin);
+  float temp = (val/1024.0)*500;
+  int delayTime = 200;
+  int pos = prev;
+  int des = (int)temp;
+  if(pos<des){ 
+   while(pos<=des){
+    motor.write(pos);
+    pos++;
+    delay(delayTime);
+   }
+  }
+  else{
+    while(pos>=des){
+      motor.write(pos);
+      pos--;
+      delay(delayTime);
+   }
+  }
+  prev = des;
+  delay(1000);
+}
